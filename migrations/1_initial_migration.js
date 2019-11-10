e6a9eb6386ae3045fcad1785d9524c3a9d0bd61c
omnipede <omnipede@naver.com>
2019-11-10 18:29:10 +0900

    truffle config 수정

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
 const Migrations = artifacts.require("Migrations");
-
+const Ballot = artifacts.require("Ballot");
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(Ballot);
 };
